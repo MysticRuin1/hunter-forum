8353486e3dc6918ac6e3e3bfb4710f20f92c098f
MysticRuin1 <alvinsookhlall@gmail.com>
2025-08-19 22:29:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // During local dev, calls to /api/* go to the backend server
-      '/api': 'http://localhost:3000'
-    }
-  },
+  root: ".",
   build: {
-    outDir: 'dist'
+    outDir: "dist"
   }
-})
+});
+
